0d7508442c4fc35fef0773d2d42df3a67657cc74
stonecodecs <slystone20@gmail.com>
2026-02-11 09:28:14 +0000

    [ADD] initial fpn face encoder implementation (untested)

--- src/model/encoder/__init__.py
+++ src/model/encoder/__init__.py
@@ -4,16 +4,17 @@ from .encoder import Encoder
 from .encoder_noposplat import EncoderNoPoSplatCfg, EncoderNoPoSplat
 from .encoder_noposplat_multi import EncoderNoPoSplatMulti
 from .encoder_template_uv_concat_bone import EncoderTemplateUVConcatBone, EncoderLBSNoPoSplatCfg
+from .encoder_template_uv_face import EncoderTemplateUVFace, EncoderLBSNoPoSplatFaceCfg
 from .visualization.encoder_visualizer import EncoderVisualizer
 
 ENCODERS = {
     "noposplat": (EncoderNoPoSplat, None),
     "noposplat_multi": (EncoderNoPoSplatMulti, None),
     "template_uv_concat_bone": (EncoderTemplateUVConcatBone, None),
+    "template_uv_face": (EncoderTemplateUVFace, None),
 }
 
-EncoderCfg = EncoderNoPoSplatCfg | EncoderLBSNoPoSplatCfg
-
+EncoderCfg = EncoderNoPoSplatCfg | EncoderLBSNoPoSplatCfg | EncoderLBSNoPoSplatFaceCfg
 
 def get_encoder(cfg: EncoderCfg) -> tuple[Encoder, Optional[EncoderVisualizer]]:
     encoder, visualizer = ENCODERS[cfg.name]
